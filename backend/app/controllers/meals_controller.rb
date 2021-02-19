8cda2ca6cdd301b70290a2030fd438a63f447b0d
Arvin Fernandez <arvinf07@gmail.com>
2021-02-19 11:20:50 -0500

    set up nested attr for meal_foods

--- backend/app/controllers/meals_controller.rb
+++ backend/app/controllers/meals_controller.rb
@@ -1,2 +1,12 @@
 class MealsController < ApplicationController
+  
+  def index
+   ## meals = Meals where created at is today
+    
+  end
+
+  private
+  def meal_params
+    params.require(:meal).permit(:name, meal_foods_attributes: [:food_id, :amount])
+  end
 end
--- backend/app/models/meal.rb
+++ backend/app/models/meal.rb
@@ -1,4 +1,6 @@
 class Meal < ApplicationRecord
   has_many :meal_foods
   has_many :foods, through: :meal_foods
+
+  accepts_nested_attributes_for :meal_foods, reject_if: proc { |attributes| attributes['amount'].blank?
 end
