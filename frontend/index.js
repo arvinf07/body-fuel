c22e6c214df634f7581b682bf6a6ffdaf6dd0346
Arvin Fernandez <arvinf07@gmail.com>
2021-02-19 10:26:21 -0500

    refactored mthod

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,7 @@ const addButtons = document.querySelectorAll('button')
 
 addButtons.forEach( e => e.addEventListener('click', renderFoodForm))
 
+getFoods()
 function getFoods(){
   fetch('http://127.0.0.1:3000/foods')
   .then(resp => resp.json())
@@ -16,14 +17,38 @@ function makeFood(json){
 function renderFoodForm(){
   this.style.visibility = 'hidden'
   let newFoodForm = document.createElement('form')
-  newFoodForm.innerHTML = 
-  `<label>Food Name</label>
-   <select name="cars" id="cars">
-    <option value="volvo">Volvo</option>
-    <option value="saab">Saab</option>
-    <option value="mercedes">Mercedes</option>
-    <option value="audi">Audi</option>
-   </select>
+  newFoodForm.innerHTML = `
+    <select name="food_name" id="food">
+    </select>
+    Quantity(in grams)
+    <input type='number' min=0 required='true'>
+    <input type="submit" value="Log Food">
   `
-    
+  this.parentElement.appendChild(newFoodForm)
+  newFoodForm.addEventListener('submit', submitHandler)
+  createDropDown()
+}
+
+function createDropDown(){
+  Food.all.forEach( food => {
+    let option = document.createElement('option')
+    option.value = food.name
+    option.innerText = food.name
+    document.getElementById('food').appendChild(option)
+  })
+}
+
+//Button does not reappear after foodlogging
+
+
+function submitHandler(e){
+  //Optimistic approach.
+  e.preventDefault()
+  let food = this.querySelector('select').value
+  let quantity = this.querySelector('input').value
+  let newRow = document.createElement('tr')
+  newRow.innerHTML = `<td>${food} - ${quantity} grams</td>`
+  document.querySelector('tbody').insertBefore(newRow, this.closest('tr'))
+  document.getElementById('breakfast-btn').style.visibility = ''
+  this.remove()
 }
