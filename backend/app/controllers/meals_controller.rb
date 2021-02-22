d2d7447080c73497b5d385048e3d602c6bc7aa5f
Arvin Fernandez <arvinf07@gmail.com>
2021-02-22 18:53:29 -0500

    fixed meal_params

--- backend/app/controllers/meals_controller.rb
+++ backend/app/controllers/meals_controller.rb
@@ -2,14 +2,18 @@ class MealsController < ApplicationController
   
   def index
     ## meals = Meals where created_at is today
-    ## formatt return data to include meal name, food name and macros, and meal_foods amount
     meals = Meal.all
     render json: meals.to_json(include: [:meal_foods])
   end 
 
+  def create
+    
+  end
+
   def update
     meal = Meal.find_by(id: params[:id])
-    meal.meal_foods.build(meal_id: meal, food_id: params['meal']['meal_foods_attributes']['food_id'].to_i, amount: params['meal']['meal_foods_attributes']['amount'].to_i )
+    meal.meal_foods.build(meal_params[:meal_foods_attributes])
+    # meal.meal_foods.build(meal_id: meal, food_id: params['meal']['meal_foods_attributes']['food_id'].to_i, amount: params['meal']['meal_foods_attributes']['amount'].to_i )
     meal.save
     render json: meal.to_json(include: {foods: {except: [:created_at, :updated_at]}})
   end
@@ -17,11 +21,11 @@ class MealsController < ApplicationController
   private
   #no implicit conversion of string into integer
   def meal_params
-    ##call switch to int here before permitting params
-    params.require(:meal).permit(:name, meal_foods_attributes: [:food_id, :amount])
+    switch_to_int
+    params.require(:meal).permit(:name, meal_foods_attributes: [:food_id, :amount, :id])
   end
 
-  def switch_to_int(params)
+  def switch_to_int
     params['meal']['meal_foods_attributes']['food_id'] = params['meal']['meal_foods_attributes']['food_id'].to_i
     params['meal']['meal_foods_attributes']['amount'] = params['meal']['meal_foods_attributes']['amount'].to_i
   end
