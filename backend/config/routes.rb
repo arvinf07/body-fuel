0853067ac19d73679223a2bf6ce10d2f78feb5ed
Arvin Fernandez <arvinf07@gmail.com>
2021-02-23 13:11:07 -0500

    added meal_foods delete route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :meals
   resources :foods
+  resources :meal_foods, only: [:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
