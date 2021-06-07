e9c9e55e96874f18792c0943cbe51e927203c187
Arvin Fernandez <arvinf07@gmail.com>
2021-06-07 19:57:29 -0400

    created sessions controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,25 +1,15 @@
 class UsersController < ApplicationController
 
-  def edit
-    current_user
-  end
-
-  def update
-
-  end
-
   def new
-    @user = User.new
+    user = User.new
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to user_workouts_path(@user)
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
     else
-      @errors = @user.errors.full_messages
-      render 'new'
+      errors = user.errors.full_messages
     end  
   end
   
@@ -27,5 +17,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :height, :weight)
   end
-  
+
 end
