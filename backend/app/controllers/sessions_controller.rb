6af07693862d82a79f3b3cad4c9e409e06d29cc1
Arvin Fernandez <arvinf07@gmail.com>
2021-06-11 09:58:56 -0400

    created sessions#create

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,6 +1,17 @@
 class SessionsController < ApplicationController
 
   def create
+    if user = User.find_by_username(params[:username])&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user.to_json({include: [:meals], except: [:created_at, :updated_at]})
+    else
+      # just do catch the error in the async function
+      redirect_to new_session_path, alert: "The password and/or email are incorrect"
+    end
   end
+
+  # def destroy
+  #   reset_session
+  # end
 
 end
